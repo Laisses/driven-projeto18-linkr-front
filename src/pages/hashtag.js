10b9247c7e33b164845012201df071a351522656
Esdras Antero <esdrasanterofontes@gmail.com>
2023-01-04 19:39:31 -0300

    feat: trending list component

--- src/pages/hashtag.js
+++ src/pages/hashtag.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { BASE_URL } from "../constants/url";
 import { Link, useParams } from "react-router-dom";
 import Header from "../constants/header";
+import TrendingList from "../components/trending";
 
 const trendings = ['react', 'html', 'css', 'js', 'angular', 'vue', 'php', 'c++', 'node', 'python']
 
@@ -20,11 +21,7 @@ export default function HashtagPage () {
                     <ul>{trendings.map((t, idx) => <li key={idx}> # {t}</li>)}</ul>
                 </LeftPart>
 
-                <RigthPart>
-                    <h1>trending</h1>
-
-                    <ul>{trendings.map((t, idx) => <Link to={`/hashtag/${t}`}><li key={idx}> # {t}</li></Link>)}</ul>
-                </RigthPart>
+                <TrendingList/>
             </Container>
         </>
     )
@@ -65,52 +62,5 @@ const LeftPart = styled.div`
         height: 276px;
         background: #171717;
         border-radius: 16px;
-    }
-`
-//"RightPart" é o componente da caixa "trending", fiz assim pra ver como fica 
-const RigthPart = styled.div`
-    background-color: #171717;
-    width: 301px;
-    height: 406px;
-    right: 260px;
-    border-radius: 16px;
-    margin-top: 155px;
-
-    h1 {
-        cursor: default;
-        padding: 9px 16px;
-        font-family: 'Oswald';
-        font-style: normal;
-        font-weight: 700;
-        font-size: 27px;
-        line-height: 40px;
-        color: #FFFFFF;
-        border-bottom: 1px solid #484848;
-    }
-
-    ul {
-        display: flex;
-        flex-direction: column;
-        gap: 8px;
-        padding: 22px 16px;
-        font-family: 'Lato';
-        font-style: normal;
-        font-weight: 700;
-        font-size: 19px;
-        line-height: 23px;
-        letter-spacing: 0.05em;
-        color: #FFFFFF;
-    }
-    
-    a { 
-        width: fit-content;
-        text-decoration: none;
-        font-family: 'Lato';
-        font-style: normal;
-        font-weight: 700;
-        font-size: 19px;
-        line-height: 23px;
-        letter-spacing: 0.05em;
-        color: #FFFFFF;
     }
 `
