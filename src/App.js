1c3282bc1c398b42a9713eb149edb852b0774e96
Laisses <laisse@gmail.com>
2023-01-04 10:19:33 -0300

    feat: add Timeline route to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GlobalStyle from "./assets/styles/GlobalStyle";
+import { Timeline } from './components/Timeline';
 
-export default function App () {
+export default function App() {
     return (
         <BrowserRouter>
-            <GlobalStyle/>
-                <Routes>
-                    <Route path="/" element={"olá"}/>
-                </Routes>
+            <GlobalStyle />
+            <Routes>
+                <Route path="/" element={"olá"} />
+                <Route path="/timeline" element={<Timeline />} />
+            </Routes>
         </BrowserRouter>
     )
 }
