47dcec04532fa7df224bc0c2e26fa0a47aac084d
Gabriel <gabriel.leite.leandro@gmail.com>
2023-01-06 11:39:08 -0300

    feat: signin & signup routes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GlobalStyle from "./assets/styles/GlobalStyle";
 import { Timeline } from './components/Timeline';
 import HashtagPage from './pages/hashtag';
+import { useState } from 'react';
+import SignIn from './components/SignIn.js'
+import SignUp from './components/SignUp.js'
+import UserContext from './contexts/userContext';
 
 export default function App() {
+
+    const [data, setData] = useState('')
+
     return (
         <BrowserRouter>
             <GlobalStyle />
-            <Routes>
-                <Route path="/" element={"olá"} />
-                <Route path="/timeline" element={<Timeline />} />
-                <Route path="/hashtag/:hashtag" element={<HashtagPage/>}/>
+            <UserContext.Provider value={{ data, setData }}>
+                <Routes>
+                    <Route path="/" element={<SignIn />} />
+                    <Route path="/sign-up" element={<SignUp />} />
+                    <Route path="/timeline" element={<Timeline />} />
+                    <Route path="/hashtag/:hashtag" element={<HashtagPage />} />
                 </Routes>
+            </UserContext.Provider>
         </BrowserRouter>
     )
 }
