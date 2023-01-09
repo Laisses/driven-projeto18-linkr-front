e503b7a68d4db9c4b4f3ec15272ce302f8593dbf
Gabriel <gabriel.leite.leandro@gmail.com>
2023-01-09 11:21:24 -0300

    feat: mobile devices layout

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -67,7 +67,12 @@ const ContainerSignin = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
-	background-color: #151515;
+	background-color: rgba(33,33,33);
+
+    @media (max-width: 1100px) {
+        display:flex;
+        flex-direction:column;
+    }
 `
 const ContainerLeft = styled.div`
     background-color:  black;
@@ -77,6 +82,14 @@ const ContainerLeft = styled.div`
     flex-direction: column;
     align-items: flex-start;
     padding-top: 250px;
+
+    @media (max-width: 1100px) {
+        width: 100%;
+        height: 40%;
+        padding-top: 40px;
+        align-items: center;
+        justify-content: center;
+    }
     
 
     p{
@@ -85,6 +98,12 @@ const ContainerLeft = styled.div`
         font-weight: 700;
         font-size: 106px;
         padding-left: 50px;
+
+        @media (max-width: 1100px) {
+        align-items:center;
+        font-size: 75px;
+        padding-left: 0px;
+    }
     }
 
     h1{
@@ -94,7 +113,14 @@ const ContainerLeft = styled.div`
         font-size: 43px;
         line-height: 66px;
         padding-left: 50px;
+
+        @media (max-width: 1100px) {
+        font-size: 30px;
+        padding-left: 35px;
+        line-height: 50px
     }
+    }
+
 `
 const ContainerRight = styled.div`
     width: 30%;
@@ -103,6 +129,10 @@ const ContainerRight = styled.div`
     flex-direction: column;
     align-items: center;
     padding-top: 150px;
+
+    @media (max-width: 1100px) {
+        margin-top: -210px;
+    }
 `
 const ContainerInput = styled.div`
 
@@ -156,7 +186,10 @@ const ContainerSwitch = styled.div`
 
     h1{
         color: white;
-        text-decoration: 1px solid white;
+        border-bottom: 1px solid white;
         
+        @media (max-width: 1100px) {
+        white-space: nowrap;
+    }
     }
 `
