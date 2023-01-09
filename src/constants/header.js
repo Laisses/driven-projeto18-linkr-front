7f909066a67a55d1171bee0f7acc8dfcbaff082e
Laisses <laisse@gmail.com>
2023-01-09 12:19:51 -0300

    feat: add responsive layout to timeline page

--- src/constants/header.js
+++ src/constants/header.js
@@ -42,47 +42,47 @@ export default function Header () {
 
             <InputContainer profiles={profiles}>
                 <img src={searchIcon} alt="Search Icon" />
-                <DebounceInput 
+                <DebounceInput
                     placeholder="Search for people"
                     minLength={3}
                     debounceTimeout={300}
-                    onChange={e => setName(e.target.value)} 
+                    onChange={e => setName(e.target.value)}
                 />
 
                 <div>
                     {profiles.map((p) => {
                         return (
-                            <StyledLink to={`/user/${p.id}`} key={p.id}> 
+                            <StyledLink to={`/user/${p.id}`} key={p.id}>
                                 <span>
                                     <img src={p.photo} alt="Profile Pic" />
                                     <h2>{p.name}</h2>
                                 </span>
                             </StyledLink>
                         )
-                    })}             
+                    })}
                 </div>
-                
+
             </InputContainer>
-            
+
             <LogoutCase>
                 {rotate ? <MdKeyboardArrowUp onClick={turnArrow}/> : <MdKeyboardArrowDown onClick={turnArrow}/>}
-                
+
                 <img onClick={turnArrow} src={data.user.photo} alt="user"/>
 
-                <LogoutDiv rotate={rotate.toString()}> 
-                    <Link 
-                        onClick={() => { 
-                            localStorage.removeItem("token"); 
+                <LogoutDiv rotate={rotate.toString()}>
+                    <Link
+                        onClick={() => {
+                            localStorage.removeItem("token");
                             localStorage.removeItem("data");
                             setToken('')
-                        }} 
+                        }}
                         to={"/"}
                     >
                         Logout
                     </Link>
                 </LogoutDiv>
 
-                <LogoutBackground rotate={rotate.toString()} onClick={turnArrow}/> 
+                <LogoutBackground rotate={rotate.toString()} onClick={turnArrow}/>
             </LogoutCase>
         </Container>
     )
@@ -118,7 +118,7 @@ const LogoutCase = styled.div`
     align-items: center;
     margin-left: 15px;
     gap: 16px;
-    
+
     svg {
         cursor: pointer;
         font-size: 35px;
@@ -144,9 +144,9 @@ const LogoutDiv = styled.div`
     border-radius: 0px 0px 0px 20px;
     cursor: pointer;
 
-    a { 
+    a {
         z-index: 2;
-        cursor: pointer;  
+        cursor: pointer;
         font-family: 'Lato';
         font-style: normal;
         font-weight: 700;
@@ -216,7 +216,7 @@ const InputContainer = styled.div`
             align-items: center;
             padding-left: 16px;
             padding: 8px;
-            
+
 
             &:hover {
                 cursor: pointer;
